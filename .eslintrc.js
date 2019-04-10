d4c6d95b7f1044205bde69175ed933a52266c149
Kim <kim.johnsson@telia.com>
2019-04-10 14:33:20 +0200

    start filer

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        'airbnb-base',
+        'plugin:vue/recommended'
+    ],
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
+    },
+    plugins: [
+        'vue',
+    ],
+    rules: {
+        "indent": ["error", 4],
+        "linebreak-style": 0
+    },
+};
---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'Hello World',
+        };
+    },
+};
+</script>
+
+<style lang="scss">
+    #app {
+        font-size: 18px;
+        font-family: "Roboto", sans-serif;
+        color: blue;
+        &:hover {
+            color: red;
+        }
+    }
+</style>
