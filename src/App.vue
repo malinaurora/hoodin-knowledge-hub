6f53724797c4903b3a766d5cfe463dd94adfdea6
SimonPalmgren <simon.palmgren91@gmail.com>
2019-04-12 10:40:10 +0200

    added component for NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
+    <nav>
+      <NavBar />
+    </nav>
     <Api />
   </div>
 </template>
 
 <script>
 import Api from './components/Api.vue';
+import NavBar from './components/NavBar.vue';
 
 export default {
     components: {
         Api,
+        NavBar,
     },
 };
-console.log(Api);
-
 </script>
 
 <style lang="scss">
