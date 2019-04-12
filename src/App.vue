e08376d1b12696fb7fdd6e452a64f5eb4e301571
Kim <kim.johnsson@telia.com>
2019-04-12 10:42:48 +0200

    Data from api props

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,29 @@
 <template>
   <div id="app">
-    <Api />
+    <Start :api-data="apiData" />
   </div>
 </template>
 
 <script>
-import Api from './components/Api.vue';
+import axios from 'axios';
+import Start from './components/Start.vue';
 
 export default {
     components: {
-        Api,
+        Start,
+    },
+    data() {
+        return {
+            apiData: null,
+        };
+    },
+    mounted() {
+        axios
+            .get('https://interns-test-channel.hoodin.com/api/v2/items?limit=50&&token=eyJpdiI6IktJMXkwWllPdzJCSzl2RE9RMmNqQ3c9PSIsInZhbHVlIjoiQ3VQQXVOV1wvVEJidmhRR1lcL0pSUE5XUmdzdE1TK2J1VlZ6TUNwYWk1enlmaERYbzR2TlJ6enZCNUI2K2l6ejVlWlFWZFQ3NDhsY1crMzl5NHlLRzN3dz09IiwibWFjIjoiMjkxYzBjY2JkMDliNmY0YjVmY2E3NGI4NTVlMTZlNDYxMWUxZGY1NTk3ZGI4MzJkZjY2NWUwMGZmM2ExYjlhNiJ9')
+            // eslint-disable-next-line no-return-assign
+            .then(response => (this.apiData = response));
     },
 };
-console.log(Api);
 
 </script>
 
---
+++ src/components/Start.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+    {{ apiData }}
+  </div>
+</template>
+
+<script>
+
+export default {
+    props: ['apiData'],
+};
+</script>
+
+<style>
+</style>
