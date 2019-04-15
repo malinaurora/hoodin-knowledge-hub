123a8ade50c44fcc6469505094323cd1c9db9df3
Kim <kim.johnsson@telia.com>
2019-04-15 12:40:18 +0200

    forgot brach

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -4,11 +4,25 @@
     <div
       v-for="data in apiData.data.data.items"
       :key="data.id"
-      class="col-12 mt-5"
+      class="col-md-4 mt-3 mb-3"
     >
-      <div>
-        {{ data }}
-      </div>
+      <article>
+        <div>
+          <iframe
+            v-if="data.video != null"
+            width="100%"
+            height="50%"
+            :src="'https://www.youtube.com/embed/' + data.video.video_id"
+          />
+          <section class="text">
+            <h2>{{ data.title }}</h2>
+            {{ data.text }}
+          </section>
+          <footer>
+            {{ data.author.name }}
+          </footer>
+        </div>
+      </article>
     </div>
   </div>
 </template>
@@ -16,9 +30,27 @@
 <script>
 
 export default {
-    props: ['apiData'],
+    props: {
+        apiData: {
+            type: Object,
+            default() {
+                return { message: 'Api Data' };
+            },
+        },
+    },
 };
 </script>
 
-<style>
+<style lang="scss">
+    article{
+        height: 400px;
+        overflow: hidden;
+        box-shadow: 0px 0px 10px gray;
+        .text{
+            padding: 5%;
+        }
+        iframe{
+            border:none;
+        }
+    }
 </style>
