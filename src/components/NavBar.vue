ad299858f5dca3b0dad499c3790177fd13bd92f4
SimonPalmgren <simon.palmgren91@gmail.com>
2019-04-15 14:47:38 +0200

    On my way fixing the navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,38 @@
 <template>
-  <div />
+  <nav>
+    <div class="sidebar">
+      <ul class="mt-5">
+        <li>
+          <img
+            src="/src/images/baseline-home.svg"
+            alt="Navigation icon home"
+          >
+          <div>Home</div>
+        </li>
+        <li>
+          <img
+            src="/src/images/baseline-favorite-border.svg"
+            alt="Navigation icon home"
+          >
+          <div>Favorite</div>
+        </li>
+        <li>
+          <img
+            src="/src/images/baseline-search.svg"
+            alt="Navigation icon home"
+          >
+          <div>Search</div>
+        </li>
+        <li>
+          <img
+            src="/src/images/filter-outline.svg"
+            alt="Navigation icon home"
+          >
+          <div>Filters</div>
+        </li>
+      </ul>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -8,6 +41,40 @@ export default {
 };
 </script>
 
-<style scoped>
-
+<style lang="scss">
+    nav{
+        float:left;
+        height: 100vh;
+        width: 50px;
+        .sidebar{
+            background-color: #F6F6F6;
+            box-shadow: 0px 6px 8px rgba(0, 0, 0, 0.25);
+            height: 100vh;
+            width: 50px;
+            position: fixed;
+        }
+        ul{
+            padding: 0;
+            margin:0;
+            li{
+                margin-top: 20px;
+                &:hover div{
+                    visibility: visible;
+                }
+                 div{
+                    position: absolute;
+                    background-color: #000;
+                    padding: 2px 6px;
+                    margin-left:40px;
+                    color:#fff;
+                    visibility: hidden;
+                }
+                img{
+                    margin: auto;
+                    width: 35px;
+                    margin-left: 7.5px
+                }
+            }
+        }
+    }
 </style>
