57e5bf81565c1f6d0c02df503318d091a65ce204
Yasmin Sakhnini <minaa0396@gmail.com>
2019-04-17 13:28:45 +0200

    remove images folder

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -28,15 +28,12 @@
       class="text-center col-md-12 .offset-md-3
  m-3"
     >
-      <button
-        type="button"
-        class="btn btn-primary"
-        data-toggle="button"
-        aria-pressed="false"
-        autocomplete="off"
-      >
-        Single toggle
-      </button>
+      <a
+        href="#"
+        class="btn btn-primary btn-lg disabled"
+        role="button"
+        aria-disabled="true"
+      >Primary link</a>
     </div>
   </div>
 </template>
