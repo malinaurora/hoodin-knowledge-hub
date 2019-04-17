3e5ea7eca6e09d525f176792a2f925360dfb4e3a
Yasmin Sakhnini <minaa0396@gmail.com>
2019-04-17 13:28:19 +0200

    added update btn

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -24,6 +24,20 @@
         </div>
       </article>
     </div>
+    <div
+      class="text-center col-md-12 .offset-md-3
+ m-3"
+    >
+      <button
+        type="button"
+        class="btn btn-primary"
+        data-toggle="button"
+        aria-pressed="false"
+        autocomplete="off"
+      >
+        Single toggle
+      </button>
+    </div>
   </div>
 </template>
 
