607787045ff1b2406ee60edc897bb9de72aef547
Kim <kim.johnsson@telia.com>
2019-04-23 21:46:27 +0200

    image slider

--- src/components/Article.vue
+++ src/components/Article.vue
@@ -4,10 +4,29 @@
       v-if="apiData.imageObjects.images.length > 0 && apiData.video === null"
       class="images"
     >
-      <img
-        :src="apiData.imageObjects.images[0].url"
-        alt="Article picture."
+      <router-link :to="{name: 'Modal', params: {id: apiData.id}}">
+        <img
+          v-for="image of apiData.imageObjects.images"
+          :key="image.url"
+          :src="image.url"
+          alt="Article picture."
+          :class="apiData.id +'Image'"
+        >
+      </router-link>
+      <button
+        v-if="apiData.imageObjects.images.length > 1"
+        class="nextImageLeft"
+        @click="nextImage(-1)"
       >
+        &#10094;
+      </button>
+      <button
+        v-if="apiData.imageObjects.images.length > 1"
+        class="nextImageRight"
+        @click="nextImage(1)"
+      >
+        &#10095;
+      </button>
     </div>
     <iframe
       v-if="apiData.video != null"
@@ -16,15 +35,16 @@
       height="50%"
       :src="'https://www.youtube.com/embed/' + apiData.video.video_id"
     />
-
-    <section class="text">
+    <router-link
+      :to="{name: 'Modal', params: {id: apiData.id}}"
+      class="text"
+    >
       <h2 v-if="apiData.title != ''">
         {{ apiData.title }}
       </h2>
       <b v-if="apiData.section !=''">{{ apiData.section }}</b>
       <p>{{ apiData.text | striphtml }}</p>
-    </section>
-
+    </router-link>
     <footer>
       <p>{{ apiData.author.name }}</p>
       <img
@@ -44,6 +64,32 @@ export default {
             default() {
                 return { message: 'Api Data' };
             },
+        },
+    },
+    data() {
+        return {
+            slideIndex: 1,
+        };
+    },
+    mounted() {
+        this.imageSlider(this.slideIndex);
+    },
+    methods: {
+        nextImage(n) {
+            this.imageSlider(this.slideIndex += n);
+        },
+        imageSlider(n) {
+            let i;
+            const x = document.getElementsByClassName(`${this.apiData.id}Image`);
+            if (n > x.length) { this.slideIndex = 1; }
+            if (n < 1) { this.slideIndex = x.length; }
+            // eslint-disable-next-line no-plusplus
+            for (i = 0; i < x.length; i++) {
+                x[i].style.display = 'none';
+            }
+            if (x[this.slideIndex - 1] !== undefined) {
+                x[this.slideIndex - 1].style.display = 'block';
+            }
         },
     },
 };
@@ -78,9 +124,44 @@ export default {
         }
         .images{
             height: 45%;
+            position: relative;
             img{
                 height: 100%;
                 width: 100%;
+            }
+            .nextImageRight{
+              position: absolute;
+              right:0;
+              top: 50%;
+              -ms-transform: translateY(-50%);
+              transform: translateY(-50%);
+              padding: 5px 10px 5px 10px;
+              border: none;
+              background: none;
+              font-size: 20px;
+              &:hover{
+                background-color: lightgray;
+              }
+              &:focus{
+                outline:0;
+              }
+            }
+            .nextImageLeft{
+              position: absolute;
+              left: 0px;
+              top: 50%;
+              -ms-transform: translateY(-50%);
+              transform: translateY(-50%);
+              padding: 5px 10px 5px 10px;
+              border: none;
+              background: none;
+              font-size: 20px;
+              &:hover{
+                background-color: lightgray;
+              }
+              &:focus{
+                outline:0;
+              }
             }
         }
         footer{
--- src/components/Start.vue
+++ src/components/Start.vue
@@ -7,11 +7,8 @@
         v-for="api of apiData"
         :key="api.id"
         class="col-lg-4 col-md-6 mt-3 mb-3"
-        @click="getArticle(api)"
       >
-        <router-link :to="{name: 'Modal', params: {id: api.id}}">
-          <Article :api-data="api" />
-        </router-link>
+        <Article :api-data="api" />
       </article>
     </div>
   </div>
@@ -27,8 +24,6 @@ export default {
     data() {
         return {
             apiData: null,
-            show: false,
-            modalArticle: null,
         };
     },
     mounted() {
@@ -42,14 +37,6 @@ export default {
                 .get('https://interns-test-channel.hoodin.com/api/v2/items?limit=15&&token=eyJpdiI6IktJMXkwWllPdzJCSzl2RE9RMmNqQ3c9PSIsInZhbHVlIjoiQ3VQQXVOV1wvVEJidmhRR1lcL0pSUE5XUmdzdE1TK2J1VlZ6TUNwYWk1enlmaERYbzR2TlJ6enZCNUI2K2l6ejVlWlFWZFQ3NDhsY1crMzl5NHlLRzN3dz09IiwibWFjIjoiMjkxYzBjY2JkMDliNmY0YjVmY2E3NGI4NTVlMTZlNDYxMWUxZGY1NTk3ZGI4MzJkZjY2NWUwMGZmM2ExYjlhNiJ9')
                 .then((response) => { this.apiData = response.body.data.items; });
         }, 60000);
-    },
-    methods: {
-        toggleModal() {
-            this.show = !this.show;
-        },
-        getArticle(articleData) {
-            this.modalArticle = articleData;
-        },
     },
 };
 </script>
