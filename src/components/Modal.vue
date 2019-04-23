c22dad3a77414e3f2bc26fbfbdd1424cce101fce
Tommy <tommynguyen@live.se>
2019-04-23 11:32:25 +0200

    Fixes whit Modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -10,11 +10,46 @@
       @click="closeCallback()"
     />
     <div class="modal_content">
-      <div />
-      <slot>
+      <div
+        v-if="modalArticle.imageObjects.images.length > 0 && modalArticle.video === null"
+        class="modalImages"
+      >
+        <img
+          :src="modalArticle.imageObjects.images[0].url"
+          alt="Article picture."
+        >
+      </div>
+      <iframe
+        v-if="modalArticle.video != null"
+        class="modalVideo"
+        width="100%"
+        height="50%"
+        :src="'https://www.youtube.com/embed/' + modalArticle.video.video_id"
+      />
+
+      <section class="modalText">
         <h1>{{ modalArticle.title }}</h1>
         <p>{{ modalArticle.text | striphtml }}</p>
-      </slot>
+      </section>
+      <footer class="modalFooter">
+        <p class="modalTime">
+          {{ modalArticle.published }}
+        </p>
+        <p class="modalAuthor">
+          {{ modalArticle.author.name }}
+        </p>
+        <a
+          class="modalOrginalArticle"
+          :v-if="modalArticle.source_url !=null"
+          :href="modalArticle.source_url"
+        >
+          Se Orginal artikeln
+        </a>
+        <img
+          src="/src/assets/icons/baseline-favorite-border.svg"
+          alt="Add to favorites."
+        >
+      </footer>
     </div>
   </div>
 </template>
@@ -33,13 +68,13 @@ export default {
 };
 </script>
 
-<style>
-  .modal {
+<style lang="scss">
+.modal {
   position: fixed;
   left: 0;
   top: 0;
   width: 100%;
-  height: 100%;
+  height: 100%
 }
 
 .overlay {
@@ -57,13 +92,14 @@ export default {
   left: 50%;
   transform: translate(-50%, -50%);
   max-height: 90%;
+  max-width: 80%;
   overflow: auto;
   background: rgb(255, 255, 255);
   box-sizing: border-box;
-  padding: 80px;
-  box-shadow: 0 1px 5px rgba(0,0,0,0.7);
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.7);
   border-radius: 4px;
-  max-width: 80%;
+  width: 950px;
+  height: 1000px;
 }
 
 .modal_content > h2 {
@@ -80,4 +116,45 @@ export default {
   right: 10px;
 }
 
+.modalImages {
+    height: auto;
+    position: relative;
+}
+
+.modalVideo {
+    border:none;
+    height: 45%;
+}
+
+.modalImages > img {
+    height: 45%;
+    width: 100%;
+}
+
+.modalText {
+    padding-top: 50px;
+    padding-right: 80px;
+    padding-left: 80px;
+    padding-bottom: 50px;
+}
+
+.modalFooter > .modalAuthor {
+  padding-left: 80px;
+  position: absolute;
+  bottom: 0px;
+  margin-bottom: 20px;
+}
+
+.modalFooter > .modalTime {
+  padding-left: 80px;
+  position: absolute;
+  bottom: 30px;
+}
+
+.modalFooter > img {
+  position: absolute;
+  bottom: 20px;
+  right: 80px;
+  width: 50px;
+}
 </style>
