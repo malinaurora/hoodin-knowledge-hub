227efb4e5385b5587cf6dc76070e4e832cade892
Yasmin Sakhnini <minaa0396@gmail.com>
2019-04-26 14:54:31 +0200

    fix

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,55 +1,51 @@
 <template>
   <div>
-    <div class="search-wrapper">
-      <input
-        v-model="search"
-        type="text"
-        placeholder="Search title.."
-        @click="searchFunc"
+    <input
+      v-model="search"
+      type="text"
+      placeholder="Search title.."
+    >
+    <div class="row mb-5">
+      <article
+        v-for="api of searchInArticles"
+        :key="api.id"
+        class="col-lg-4 col-md-6 mt-3 mb-3"
       >
-    </div>
-    <div class="wrapper">
-      <div>
-        <ul id="myUL">
-          <li>{{ apiData.title }}</li>
-        </ul>
-      </div>
+        <Article :api-data="api" />
+      </article>
     </div>
   </div>
 </template>
 
 <script>
+import Article from './Article.vue';
+
+
 export default {
+
+    components: {
+        Article,
+    },
     data() {
         return {
             search: '',
-            apiData: {},
-            articlesList: [],
-
-
+            apiData: [],
         };
+    },
+    computed: {
+        searchInArticles() {
+            return this.apiData.filter(api => api.title.match(this.search));
+        },
+
     },
     mounted() {
         this.$http
-            .get('https://interns-test-channel.hoodin.com/api/v2/items?limit=15&&token=eyJpdiI6IktJMXkwWllPdzJCSzl2RE9RMmNqQ3c9PSIsInZhbHVlIjoiQ3VQQXVOV1wvVEJidmhRR1lcL0pSUE5XUmdzdE1TK2J1VlZ6TUNwYWk1enlmaERYbzR2TlJ6enZCNUI2K2l6ejVlWlFWZFQ3NDhsY1crMzl5NHlLRzN3dz09IiwibWFjIjoiMjkxYzBjY2JkMDliNmY0YjVmY2E3NGI4NTVlMTZlNDYxMWUxZGY1NTk3ZGI4MzJkZjY2NWUwMGZmM2ExYjlhNiJ9')
+            .get('https://interns-test-channel.hoodin.com/api/v2/items?&&token=eyJpdiI6IktJMXkwWllPdzJCSzl2RE9RMmNqQ3c9PSIsInZhbHVlIjoiQ3VQQXVOV1wvVEJidmhRR1lcL0pSUE5XUmdzdE1TK2J1VlZ6TUNwYWk1enlmaERYbzR2TlJ6enZCNUI2K2l6ejVlWlFWZFQ3NDhsY1crMzl5NHlLRzN3dz09IiwibWFjIjoiMjkxYzBjY2JkMDliNmY0YjVmY2E3NGI4NTVlMTZlNDYxMWUxZGY1NTk3ZGI4MzJkZjY2NWUwMGZmM2ExYjlhNiJ9')
             .then((response) => {
                 this.apiData = response.body.data.items;
             });
     },
-    methods: {
-        searchFunc() {
-            // return this.articlesList.filter(
-            //     search: () => {
-            // 		this.apiData.title.toLowerCase().includes(
-            // 		this.search.toLowerCase(),
 
-
-            // 	),
-            // 	}
-            // );
-        },
-
-    },
 };
 
 
