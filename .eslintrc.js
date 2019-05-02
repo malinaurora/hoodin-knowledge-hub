358f0be61e4a2076d0707778d0b480bffc2a2914
Isac Ljung <isac@hoodin.com>
2019-05-02 15:33:54 +0200

    Elint fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,29 @@
 module.exports = {
+    env: {
+        es6: true,
+        browser: true,
+    },
     extends: [
         'airbnb-base',
         'plugin:vue/recommended',
-        'prettier'
+        'prettier',
+        'prettier/vue',
     ],
+    plugins: [
+        'prettier',
+        'vue',
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module',
+    },
+    rules: {
+        "prettier/prettier": ['error'],
+        indent: ['error', 4],
+        "no-console": ["error", { "allow": ["warn", "error"] }],
+        "vue/html-indent": ['error', 4],
+        eqeqeq: ['error', 'smart'],
+        "vue/eqeqeq": ['error'],
+        "vue/require-default-prop": ['error'],
+    }
 };
