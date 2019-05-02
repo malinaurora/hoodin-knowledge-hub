fb2828fb85a7ff54f513107f804df414298f7d4d
SimonPalmgren <simon.palmgren91@gmail.com>
2019-05-02 17:44:52 +0200

    Yarn fixes + added my new component

--- src/view/Start.vue
+++ src/view/Start.vue
@@ -1,23 +1,22 @@
-
 <template>
-  <div>
-    <div class="row mb-5 mt-4">
-      <article
-        v-for="api of searchInArticles"
-        :key="api.id"
-        class="col-lg-4 col-md-6 mt-3 mb-3"
-      >
-        <Article
-          :api-data="api"
-          modal-route="modalStart"
-          @saveArticleId="saveFavorites($event)"
-          @removeArticleId="removeFavorite($event)"
-        />
-      </article>
+    <div>
+        <div class="row mb-5 mt-4">
+            <article
+                v-for="api of searchInArticles"
+                :key="api.id"
+                class="col-lg-4 col-md-6 mt-3 mb-3"
+            >
+                <Article
+                    :api-data="api"
+                    modal-route="modalStart"
+                    @saveArticleId="saveFavorites($event)"
+                    @removeArticleId="removeFavorite($event)"
+                />
+            </article>
+        </div>
+        <router-view />
+        <MoreArticles @showMore="showMore($event)" />
     </div>
-    <router-view />
-    <MoreArticles @showMore="showMore($event)" />
-  </div>
 </template>
 
 <script>
@@ -46,29 +45,42 @@ export default {
         searchInArticles() {
             const searchLowerCase = this.searchString.toLowerCase();
             return this.apiData.filter(
-                api => api.title.toLowerCase().match(searchLowerCase)
-          || api.author.name.toLowerCase().match(searchLowerCase)
-          || api.subtitle.toLowerCase().match(searchLowerCase)
-          || api.text.toLowerCase().match(searchLowerCase),
+                api =>
+                    api.title.toLowerCase().match(searchLowerCase) ||
+                    api.author.name.toLowerCase().match(searchLowerCase) ||
+                    api.subtitle.toLowerCase().match(searchLowerCase) ||
+                    api.text.toLowerCase().match(searchLowerCase),
             );
         },
     },
     mounted() {
-        fetch(`https://interns-test-channel.hoodin.com/api/v2/items?limit=${this.limit}&&token=eyJpdiI6IktJMXkwWllPdzJCSzl2RE9RMmNqQ3c9PSIsInZhbHVlIjoiQ3VQQXVOV1wvVEJidmhRR1lcL0pSUE5XUmdzdE1TK2J1VlZ6TUNwYWk1enlmaERYbzR2TlJ6enZCNUI2K2l6ejVlWlFWZFQ3NDhsY1crMzl5NHlLRzN3dz09IiwibWFjIjoiMjkxYzBjY2JkMDliNmY0YjVmY2E3NGI4NTVlMTZlNDYxMWUxZGY1NTk3ZGI4MzJkZjY2NWUwMGZmM2ExYjlhNiJ9`)
+        fetch(
+            `https://interns-test-channel.hoodin.com/api/v2/items?limit=${
+                this.limit
+            }&&token=eyJpdiI6IktJMXkwWllPdzJCSzl2RE9RMmNqQ3c9PSIsInZhbHVlIjoiQ3VQQXVOV1wvVEJidmhRR1lcL0pSUE5XUmdzdE1TK2J1VlZ6TUNwYWk1enlmaERYbzR2TlJ6enZCNUI2K2l6ejVlWlFWZFQ3NDhsY1crMzl5NHlLRzN3dz09IiwibWFjIjoiMjkxYzBjY2JkMDliNmY0YjVmY2E3NGI4NTVlMTZlNDYxMWUxZGY1NTk3ZGI4MzJkZjY2NWUwMGZmM2ExYjlhNiJ9`,
+        )
             .then(response => response.json())
-            .then((data) => { this.apiData = data.data.items; });
+            .then(data => {
+                this.apiData = data.data.items;
+            });
 
         setInterval(() => {
-            fetch(`https://interns-test-channel.hoodin.com/api/v2/items?limit=${this.limit}&&token=eyJpdiI6IktJMXkwWllPdzJCSzl2RE9RMmNqQ3c9PSIsInZhbHVlIjoiQ3VQQXVOV1wvVEJidmhRR1lcL0pSUE5XUmdzdE1TK2J1VlZ6TUNwYWk1enlmaERYbzR2TlJ6enZCNUI2K2l6ejVlWlFWZFQ3NDhsY1crMzl5NHlLRzN3dz09IiwibWFjIjoiMjkxYzBjY2JkMDliNmY0YjVmY2E3NGI4NTVlMTZlNDYxMWUxZGY1NTk3ZGI4MzJkZjY2NWUwMGZmM2ExYjlhNiJ9`)
+            fetch(
+                `https://interns-test-channel.hoodin.com/api/v2/items?limit=${
+                    this.limit
+                }&&token=eyJpdiI6IktJMXkwWllPdzJCSzl2RE9RMmNqQ3c9PSIsInZhbHVlIjoiQ3VQQXVOV1wvVEJidmhRR1lcL0pSUE5XUmdzdE1TK2J1VlZ6TUNwYWk1enlmaERYbzR2TlJ6enZCNUI2K2l6ejVlWlFWZFQ3NDhsY1crMzl5NHlLRzN3dz09IiwibWFjIjoiMjkxYzBjY2JkMDliNmY0YjVmY2E3NGI4NTVlMTZlNDYxMWUxZGY1NTk3ZGI4MzJkZjY2NWUwMGZmM2ExYjlhNiJ9`,
+            )
                 .then(response => response.json())
-                .then((data) => { this.apiData = data.data.items; });
+                .then(data => {
+                    this.apiData = data.data.items;
+                });
         }, 60000);
 
         /* convert local Storage from string to array */
         const data = JSON.parse(localStorage.getItem('id'));
 
         /* push old favorites to favorites array */
-        data.forEach((id) => {
+        data.forEach(id => {
             this.Favorites.push(id);
         });
     },
@@ -77,14 +89,13 @@ export default {
             /* push new favorit id to array and saves it localy */
             this.Favorites.push(id);
             localStorage.setItem('id', JSON.stringify(this.Favorites));
-            console.log(this.searchString);
         },
         removeFavorite(removedId) {
             /* removes the id from start favorites array so it doesn't
             push same article multiple times */
             let index = 0;
 
-            this.Favorites.forEach((id) => {
+            this.Favorites.forEach(id => {
                 if (removedId === id) {
                     this.Favorites.splice(index, 1);
                 }
@@ -94,19 +105,25 @@ export default {
         showMore(limit) {
             this.limit = limit;
 
-            fetch(`https://interns-test-channel.hoodin.com/api/v2/items?limit=${this.limit}&&token=eyJpdiI6IktJMXkwWllPdzJCSzl2RE9RMmNqQ3c9PSIsInZhbHVlIjoiQ3VQQXVOV1wvVEJidmhRR1lcL0pSUE5XUmdzdE1TK2J1VlZ6TUNwYWk1enlmaERYbzR2TlJ6enZCNUI2K2l6ejVlWlFWZFQ3NDhsY1crMzl5NHlLRzN3dz09IiwibWFjIjoiMjkxYzBjY2JkMDliNmY0YjVmY2E3NGI4NTVlMTZlNDYxMWUxZGY1NTk3ZGI4MzJkZjY2NWUwMGZmM2ExYjlhNiJ9`)
+            fetch(
+                `https://interns-test-channel.hoodin.com/api/v2/items?limit=${
+                    this.limit
+                }&&token=eyJpdiI6IktJMXkwWllPdzJCSzl2RE9RMmNqQ3c9PSIsInZhbHVlIjoiQ3VQQXVOV1wvVEJidmhRR1lcL0pSUE5XUmdzdE1TK2J1VlZ6TUNwYWk1enlmaERYbzR2TlJ6enZCNUI2K2l6ejVlWlFWZFQ3NDhsY1crMzl5NHlLRzN3dz09IiwibWFjIjoiMjkxYzBjY2JkMDliNmY0YjVmY2E3NGI4NTVlMTZlNDYxMWUxZGY1NTk3ZGI4MzJkZjY2NWUwMGZmM2ExYjlhNiJ9`,
+            )
                 .then(response => response.json())
-                .then((data) => { this.apiData = data.data.items; });
+                .then(data => {
+                    this.apiData = data.data.items;
+                });
         },
     },
 };
 </script>
 
 <style lang="scss">
-article{
-    a{
+article {
+    a {
         color: black;
-        &:hover{
+        &:hover {
             color: black;
             text-decoration: none;
         }
