8721f060076a9b415d0d26f01724eb2423a13f47
SimonPalmgren <simon.palmgren91@gmail.com>
2019-05-06 11:09:07 +0200

    removed categories from template

--- src/view/Start.vue
+++ src/view/Start.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        {{ checkedCategoriesArray }}
         <div class="row mb-5 mt-4">
             <article v-for="api of apiData" :key="api.id" class="col-lg-4 col-md-6 mt-3 mb-3">
                 <Article
